268ccca4c84c70cdb80bd792c3a6e2b2ac39213a
OCSBAnthonyj <113910453+OCSBAnthonyj@users.noreply.github.com>
2022-10-31 09:53:22 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(0)
+        basic.showNumber(1 + (2 + (3 + (4 + (5 + (6 + (7 + (8 + (9 + 10)))))))))
+    }
+})
+basic.forever(function () {
+	
+})
